4347a5b650301f050001f26c5bf84f5e0bfaebb7
Doctor11 <dimazaharov0@mail.ru>
2024-01-31 20:12:20 +0500

    change url prefix

--- user/router.py
+++ user/router.py
@@ -6,10 +6,10 @@ from user.models import User
 from .service import UserService
 from user.schemas import UserCreate, UserOut
 
-user_router = APIRouter(prefix="/user", tags=["user"])
+user_router = APIRouter(prefix="/users", tags=["users"])
 
 
-@user_router.post("/signup", response_model=UserOut)
+@user_router.post("/signup", response_model=UserOut, status_code=201)
 async def signup(user: UserCreate,
                  user_service: UserService = Depends()):
     user = await user_service.signup(user)
